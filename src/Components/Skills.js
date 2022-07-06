ef030d2873fd0a064096dc4d187869d26a35474c
vantheman2210 <martinovic.ba@gmail.com>
2022-07-06 09:48:16 +0200

    building print pdf function

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,25 +1,44 @@
-import React from 'react'; 
+import React from 'react';
 import uniqid from 'uniqid';
 
 class Skills extends React.Component {
 	constructor(props) {
 		super(props);
+	} 
+
+	editSkill = (e) => { 
+		console.log(e.currentTarget.id)
 	}
 	render() {
 		const { skills } = this.props;
-		
+    
 		return (
-			<div  className="skillsDiv">
+			<div className="skillsDiv">
 				{skills.map((skill) => {
 					return (
 						<div key={uniqid()} className="skillsCV">
 							<div>
-								<p className='categoryCV'>
+								<p
+									contentEditable="true"
+			
+									className="categoryCV"
+								>
 									{skill.category}:
 								</p>
-								<p>{skill.subjectSkill}</p>
+								<p
+									contentEditable="true"
+									
+								>
+									{skill.subjectSkill}
+								</p>
 							</div>
-							<p className='levelCV'>{skill.level}</p>
+							<p
+								contentEditable="true"
+								
+								className="levelCV"
+							>
+								{skill.level}
+							</p>
 						</div>
 					);
 				})}
